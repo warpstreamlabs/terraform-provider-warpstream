ca628f43f8ee5c4050e9025fcb3c184c397a7611
RP Rectangle <romain@rectangle.ai>
2024-01-09 18:07:14 +0100

    Fix agent pool name

--- internal/provider/virtual_cluster_resource_test.go
+++ internal/provider/virtual_cluster_resource_test.go
@@ -9,17 +9,22 @@ import (
 )
 
 func TestAccVirtualClusterResource(t *testing.T) {
+	// We add a random suffix at the end of the virtual cluster name
+	// in order to prevent name collision when acceptance tests run
+	// in parallel for different terraform version.
+	suffix := acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum)
+
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Read testing
 			{
-				Config: testAccVirtualClusterResourceConfig(),
+				Config: testAccVirtualClusterResourceConfig(suffix),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("warpstream_virtual_cluster.test", "id"),
 					resource.TestCheckResourceAttrSet("warpstream_virtual_cluster.test", "agent_pool_id"),
 					resource.TestCheckResourceAttrSet("warpstream_virtual_cluster.test", "created_at"),
-					resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "agent_pool_name", "apn_test_acc"),
+					resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "agent_pool_name", "apn_test_acc_"+suffix),
 					resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "default", "false"),
 				),
 			},
@@ -27,11 +32,7 @@ func TestAccVirtualClusterResource(t *testing.T) {
 	})
 }
 
-func testAccVirtualClusterResourceConfig() string {
-	// We add a random suffix at the end of the virtual cluster name
-	// in order to prevent name collision when acceptance tests run
-	// in parallel for different terraform version.
-	suffix := acctest.RandStringFromCharSet(6, acctest.CharSetAlphaNum)
+func testAccVirtualClusterResourceConfig(suffix string) string {
 	return providerConfig + fmt.Sprintf(`
 resource "warpstream_virtual_cluster" "test" {
 	name = "vcn_test_acc_%s"
