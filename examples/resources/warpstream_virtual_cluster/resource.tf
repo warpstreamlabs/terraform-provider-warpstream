edcecd09956da026fbdbfc36b24eeeb730c8afd1
RP Rectangle <romain@rectangle.ai>
2024-01-09 18:59:43 +0100

    Add example and update doc

--- examples/resources/warpstream_virtual_cluster/resource.tf
+++ examples/resources/warpstream_virtual_cluster/resource.tf
@@ -1,3 +1,10 @@
 resource "warpstream_virtual_cluster" "test" {
   name = "vcn_test"
 }
+
+resource "warpstream_virtual_cluster" "test_with_acls" {
+  name = "vcn_test_acls"
+  configuration = {
+    enable_acls = true
+  }
+}
