6a20411a2a24025ff936cc80afa1fd9a94bc8fad
RP Rectangle <romain@rectangle.ai>
2024-01-11 16:07:28 +0100

    Validate credentials name

--- internal/provider/virtual_cluster_credentials_resource.go
+++ internal/provider/virtual_cluster_credentials_resource.go
@@ -3,11 +3,14 @@ package provider
 import (
 	"context"
 	"fmt"
+	"regexp"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/warpstreamlabs/terraform-provider-warpstream/internal/provider/api"
 )
@@ -80,6 +83,12 @@ This resource allows you to create and delete virtual cluster credentials.
 				Required:    true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.RequiresReplace(),
+				},
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^ccn_`),
+						"must start with 'ccn_' prefix",
+					),
 				},
 			},
 			"agent_pool": schema.StringAttribute{
