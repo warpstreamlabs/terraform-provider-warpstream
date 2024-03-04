142203f6d1271290adc68e858e351ed8125b1aa5
RP Rectangle <romain@rectangle.ai>
2024-03-04 10:50:13 +0100

    Set byoc as default cluster type

--- internal/provider/virtual_cluster_resource_test.go
+++ internal/provider/virtual_cluster_resource_test.go
@@ -77,7 +77,7 @@ func testAccVirtualClusterResourceCheck(acls bool, autoTopic bool, numParts int6
 		// Note: agent_pool_name is now equal to "apn_test_acc_"+nameSuffix + randomSuffix
 		resource.TestCheckResourceAttrSet("warpstream_virtual_cluster.test", "agent_pool_name"),
 		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "default", "false"),
-		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "type", "serverless"),
+		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "type", "byoc"),
 		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "configuration.enable_acls", fmt.Sprintf("%t", acls)),
 		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "configuration.auto_create_topic", fmt.Sprintf("%t", autoTopic)),
 		resource.TestCheckResourceAttr("warpstream_virtual_cluster.test", "configuration.default_num_partitions", fmt.Sprintf("%d", numParts)),
