84d525083f223586a4edd479399cb80b08c92657
RP Rectangle <romain@rectangle.ai>
2024-04-29 11:51:05 +0200

    Fix import command

--- examples/resources/warpstream_virtual_cluster/import.sh
+++ examples/resources/warpstream_virtual_cluster/import.sh
@@ -1,2 +1,2 @@
 # Virtual Cluster can be imported by specifying the identifier.
-terraform import virtual_cluster.example vci_XXXXXXXXXX
+terraform import warpstream_virtual_cluster.example vci_XXXXXXXXXX
