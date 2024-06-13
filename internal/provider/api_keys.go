f6a65e4a9a90e81c8c1b430a8fc741116b088261
Simon C <3833952+7imon7ays@users.noreply.github.com>
2024-06-13 13:46:24 +0200

    Remove api key model to consolidate on agent key model

--- internal/provider/api_keys.go
+++ internal/provider/api_keys.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/warpstreamlabs/terraform-provider-warpstream/internal/provider/api"
 )
@@ -13,30 +14,42 @@ type agentKeyModel struct {
 	CreatedAt        types.String `tfsdk:"created_at"`
 }
 
-type apiKeyModel struct {
-	Name      types.String `tfsdk:"name"`
-	Key       types.String `tfsdk:"key"`
-	CreatedAt types.String `tfsdk:"created_at"`
-}
-
-func mapToAPIKeyModels(apiKeysPtr *[]api.APIKey) *[]apiKeyModel {
+func mapToAgentKeyModels(apiKeysPtr *[]api.APIKey, diags *diag.Diagnostics) (*[]agentKeyModel, bool) {
 	if apiKeysPtr == nil {
 		// Null for Serverless clusters.
-		return nil
+		return nil, true
 	}
 
 	apiKeys := *apiKeysPtr
 
-	keyModels := make([]apiKeyModel, 0, len(apiKeys))
+	keyModels := make([]agentKeyModel, 0, len(apiKeys))
 	for _, key := range apiKeys {
-		keyModel := apiKeyModel{
-			Name:      types.StringValue(key.Name),
-			Key:       types.StringValue(key.Key),
-			CreatedAt: types.StringValue(key.CreatedAt),
+		vcID, ok := getVirtualClusterID(key, diags)
+		if !ok {
+			return nil, false // Diagnostics handled by helper.
+		}
+		keyModel := agentKeyModel{
+			ID:               types.StringValue(key.ID),
+			Name:             types.StringValue(key.Name),
+			Key:              types.StringValue(key.Key),
+			VirtualClusterID: types.StringValue(vcID),
+			CreatedAt:        types.StringValue(key.CreatedAt),
 		}
 
 		keyModels = append(keyModels, keyModel)
 	}
 
-	return &keyModels
+	return &keyModels, true
+}
+
+func getVirtualClusterID(apiKey api.APIKey, diags *diag.Diagnostics) (string, bool) {
+	if len(apiKey.AccessGrants) == 0 {
+		diags.AddError(
+			"Error Reading WarpStream Agent Key",
+			"API returned invalid Agent Key with ID "+apiKey.ID+": no access grants found",
+		)
+		return "", false
+	}
+
+	return apiKey.AccessGrants[0].ResourceID, true
 }
