60930044b5bc872e7405b679bb3b855142fa79ae
Brian Shih <brianshihstart@gmail.com>
2025-01-13 21:38:27 -0800

    add example

--- examples/resources/warpstream_pipeline/resource.tf
+++ examples/resources/warpstream_pipeline/resource.tf
@@ -34,3 +34,32 @@ resource "warpstream_pipeline" "example_pipeline" {
           topic: "test_topic_capitalized"
   EOT
 }
+
+resource "warpstream_pipeline" "example_orbit_pipeline" {
+  virtual_cluster_id = warpstream_virtual_cluster.tf_example_pipelines.id
+  name               = "example_pipeline"
+  state              = "running"
+  type               = "orbit"
+  configuration_yaml = <<EOT
+  source_bootstrap_brokers:
+    - hostname: localhost
+      port: 9092
+
+  source_cluster_credentials:
+    sasl_mechanism: plain
+    use_tls: false
+
+  topic_mappings:
+    - source_regex: topic.*
+      destination_prefix: ""
+
+  cluster_config:
+    copy_source_cluster_configuration: false
+
+  consumer_groups:
+    copy_offsets_enabled: true             
+
+  warpstream:
+    cluster_fetch_concurrency: 2
+  EOT
+}
